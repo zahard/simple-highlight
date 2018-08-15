af94bad8bd9c5e02f2cbb43c3f9c08345f943f8c
Zakhar Portianov <zakhar.portyanov@logmein.com>
2018-08-15 13:40:43 +0300

    new styles and css

--- app.js
+++ app.js
@@ -1,57 +1,119 @@
 
-var code = document.querySelector('#source');
-var text = code.innerText;
+function appendStyles() {
+  var cssArr = [];
+  cssArr.push('code.simple-highlight { font-size: 16px; padding: 15px; display: block;}');
+  cssArr.push('.simple-highlight > pre { margin: 0}');
+  cssArr.push('.simple-highlight.sh-theme-dark { background: #282923}');
+  cssArr.push('.simple-highlight.sh-theme-dark > pre {color: #f7f7f7}');
 
-var lines = text.split('\n');
-var finalLines = [];
+  var css = cssArr.join('\n');
+  var head = document.head || document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+  style.type = 'text/css';
+  if (style.styleSheet){
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  head.appendChild(style);
+}
+
+appendStyles()
 
 var keywords = {
-  'class': 'color:blue;font-style: italic',
-  'function': 'color:blue;font-style: italic',
+  'class': 'color:#67d8ef;font-style: italic',
+  'function': 'color:#67d8ef;font-style: italic',
   'this': 'color:orange',
-  'constructor': 'color:blue;font-style: italic',
-  'return' :'color:red',
-  'if': 'color:red',
-  'else': 'color:red',
-  'const': 'color: blue',
-  'new': 'color:red',
-  'var': 'color:blue',
+  'constructor': 'color:#67d8ef;font-style: italic',
+  'document': 'color:#67d8ef;font-style: italic',
+  'window': 'color:#67d8ef;font-style: italic',
+  'return' :'color:#f92472',
+  'if': 'color:#f92472',
+  'else': 'color:#f92472',
+  'const': 'color: #67d8ef',
+  'new': 'color:#f92472',
+  'var': 'color: #67d8ef',
 }
 
 
 var modifiers = {
-  'class': 'color:green',
-  'function': 'color:green',
-  'new': 'color:blue'
+  'class': 'color:#a6e22c',
+  'function': 'color:#a6e22c',
+  'new': 'color:#67d8ef;font-style: italic'
 }
 
+var styles = {
+  //args: 'color: #fd9621'
+}
+
+var openBlockLevel = 0;
+var wordString; 
+var classLevel;
+var isClass = false;
+var isArguments = false;
+var isFunctionDefinition = false;
+
+var code = document.querySelector('code pre');
+var text = code.innerText;
+var lines = text.split('\n');
+var finalLines = [];
 for (var i = 0; i < lines.length; i++) {
   finalLines.push(processLine(lines[i]));
 }
+code.innerHTML = finalLines.join('\n');
+
+
+function isClassMethod() {
+  return isClass && classLevel === openBlockLevel;
+}
 
 function getWord(word, customStyle) {
-  var style = keywords[word] || customStyle;
-  // Numbers
-  if (/^[0-9]+$/.test(word)) {
-    style = 'color: red'
-  // Strings
-  } else if(/^[\'\"].*[\'\"]+$/.test(word)) {
-    style = 'color: green'
+  var style = keywords[word] || customStyle; 
+  if (!style) {
+    if (isArguments) {
+      style = 'color: #fd9621';
+    } else  if (isClassMethod()) {
+      // Methods
+      style = 'color: #a6e22c';
+    } else if (/^[0-9]+$/.test(word)) {
+      // Numbers
+      style = 'color: #ac80ff'
+    } else if(/^[\'\"].*[\'\"]+$/.test(word)) {
+      // Strings
+      style = 'color: #e7db74'
+    }
   }
 
   if (style) {
-    return `<span style="${style}">${word}</span>`;
+    return applyStyle(word, style);
   } else {
     return word;
   }
 }
+
+function applyStyle(word, style) {
+  return ['<span style="',style,'">',word,'</span>'].join('');
+}
+
 function nextWordRule(keyword) {
-  return modifiers[keyword]
+  if (modifiers.hasOwnProperty(keyword)) {
+    return modifiers[keyword];
+  } else {
+    return null;
+  }
 }
 
 function isWordChar(char) {
   return /[a-zA-Z0-9\_\-\'\"]/.test(char)
 };
+
+function getCharStyle(char) {
+  if (/^[\+\-\*\|\/\>\<\=]+$/.test(char)) {
+    return applyStyle(char, 'color:#f92472');
+  } else {
+    return char;
+  }
+}
 
 function processLine(line) {
   var char;
@@ -71,18 +133,67 @@ function processLine(line) {
     } else {
       if (wordStart) {
         // End of word
-        processed.push(getWord(word.join(''), nextWordModifier));
-        nextWordModifier = nextWordRule(word.join(''));
+        wordString = word.join('');
+        processed.push(getWord(wordString, nextWordModifier));
+        nextWordModifier = nextWordRule(wordString);
         wordStart = false;
         word = [];
+
+        // Process next words in current block leve as method names
+        if (wordString === 'class') {
+          classLevel = openBlockLevel + 1;
+          isClass = true;
+        }
+
+        if (wordString === 'function') {
+          isFunctionDefinition = true;
+        }
       }
-      processed.push(char);
+
+      if (isClassMethod() || isFunctionDefinition) {
+        if (char === '(') {
+          isArguments = true;
+        } else if (char === ')') {
+          isArguments = false;
+          isFunctionDefinition = false;
+        }
+      }
+
+      if (char == '{') {
+        openBlockLevel++;
+      } else if(char == '}') {
+        if (isClass && classLevel == openBlockLevel) {
+          isClass = false;
+        }
+        openBlockLevel--;
+      }
+
+      // Check for arrow function
+      if (char === '=') {
+        if (line[i+1] && line[i+1] === '>') {
+          var nextChar;
+          var k = 1;
+          while(!nextChar) {
+            if (! (/\s/.test(line[i+1+k]))) {
+              nextChar = line[i+1+k]
+            } else {
+              k++;
+            }
+          }
+
+          // Is arrow function
+          if (nextChar === '{') {
+            processed.push('=>');
+            //skip next char
+            i++;
+            continue;
+          }
+        }
+      }
+
+      processed.push(getCharStyle(char));
     }
   }
 
   return processed.join('');
 }
-
-
-var html = finalLines.join('\n');
-document.querySelector('#dest').innerHTML = html;
