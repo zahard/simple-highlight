1d66cd88e5e11638b8b36e71390e88de2869a76e
Zakhar Portianov <zakhar.portyanov@logmein.com>
2018-08-15 17:00:12 +0300

    Add styles and minifed version

--- simple-highlight.js
+++ simple-highlight.js
@@ -1,127 +1,241 @@
-var themes = [{
-  name: 'dark',
-  background: '#282923',
-  text: '#f7f7f7',
-  colors: {
-    orange: '#fd9621',
-    green: '#a6e22c',
-    purple: '#ac80ff',
-    yellow: '#e7db74',
-    red: '#f92472',
-    blue: '#67d8ef',
-  },
-  styles: {
-    def: 'blue',
-    args: 'orange',
-    method: 'green',
-    number: 'purple',
-    string: 'yellow',
-    operand: 'red',
-    keyword: 'red',
-    b_name: 'blue|i',
-    g_name: 'blue|i'
-  }
-}];
+!(function(globalObject) {
 
-themes.push({
-  name: 'light',
-  background: '#eee',
-  text: '#222',
-  colors: {
-    orange: '#fd9621',
-    green: '#a6e22c',
-    purple: '#ac80ff',
-    yellow: '#e7db74',
-    red: '#f92472',
-    blue: '#67d8ef',
-    grey: '#555'
+var SimpleHighlight = {
+  mainClass: '.simple-highlight',
+  themePrefix: 'sh-theme-',
+  keywords: {
+    'class': 'b_name',
+    'function': 'b_name',
+    'this': 'self',
+    'constructor': 'b_name',
+    'document': 'g_name',
+    'window': 'g_name',
+    'console': 'g_name',
+    'return' :'keyword',
+    'if': 'keyword',
+    'else': 'keyword',
+    'const': 'def',
+    'var': 'def',
+    'let': 'def',
+    'new': 'keyword',
   },
-  styles: {
-    def: 'red',
-    args: 'orange|b',
-    method: 'purple',
-    number: 'green',
-    string: 'yellow',
-    operand: 'red',
-    keyword: 'red',
-    b_name: 'grey|b',
-    g_name: 'grey|i'
-  }
-});
+  modifiers: {
+    'class': 'method',
+    'function': 'method',
+    'new': 'g_name',
+    'console': 'g_name',
+  },
 
-var keywords = {
-  'class': 'b_name',
-  'function': 'b_name',
-  'this': 'self',
-  'constructor': 'b_name',
-  'document': 'g_name',
-  'window': 'g_name',
-  'console': 'g_name',
-  'return' :'keyword',
-  'if': 'keyword',
-  'else': 'keyword',
-  'const': 'def',
-  'var': 'def',
-  'let': 'def',
-  'new': 'keyword',
+  openBlockLevel: 0,
+  classLevel: null,
+  isClass: false,
+  isArguments: false,
+  isFunctionDefinition: false,
 };
 
-var modifiers = {
-  'class': 'method',
-  'function': 'method',
-  'new': 'g_name',
-  'console': 'g_name',
-};
 
-var openBlockLevel = 0;
-var classLevel;
-var isClass = false;
-var isArguments = false;
-var isFunctionDefinition = false;
-
-highlightCode();
-
-function highlightCode() {
-  var codeNodes = document.querySelectorAll('code.simple-highlight');
-  if (!codeNodes.length) {
+SimpleHighlight.highlightCodeNode = function(codeNode) {
+  var preNode = codeNode.firstElementChild;
+  // If pre tag wasnt found inside code
+  if (preNode.tagName.toLowerCase() !== 'pre') {
     return;
   }
-  
-  appendStyles(themes);
 
-  for (var i = 0; i < codeNodes.length; i++) {
-    var codeNode = codeNodes[i];
-    var preNode = codeNode.firstElementChild;
+  this.reset();
 
-    if (preNode.tagName.toLowerCase() !== 'pre') {
-      continue;
-    }
+  var lines = preNode.innerText.split('\n');
+  var readyLines = [];
+  for (var line = 0; line < lines.length; line++) {
+    readyLines.push(this.processLine(lines[line]));
+  }
+  preNode.innerHTML = readyLines.join('\n');
 
-    var lines = preNode.innerText.split('\n');
-    var finalLines = [];
-    for (var line = 0; line < lines.length; line++) {
-      finalLines.push(processLine(lines[line]));
-    }
-    preNode.innerHTML = finalLines.join('\n');
-
-    //If no theme selected - apply default
-    if (codeNode.className.indexOf('sh-theme-') === -1) {
-      codeNode.className = codeNode.className + ' sh-theme-light';
-    }
+  //If no theme selected - apply default
+  if (codeNode.className.indexOf(this.themePrefix) === -1) {
+    codeNode.className = codeNode.className + ' ' + this.themePrefix + 'light';
   }
 }
 
-function appendStyles(themes) {
+SimpleHighlight.highlightOnPage = function() {
+  // Appned styles
+  this.appendStyles(this.themes);
+
+  var codeNodes = document.querySelectorAll('code' + this.mainClass);
+  if (!codeNodes.length) {
+    return;
+  }
+
+  for (var i = 0; i < codeNodes.length; i++) {
+    this.highlightCodeNode(codeNodes[i]);
+  }
+};
+
+SimpleHighlight.processLine = function(line) {
+  var char;
+  var code;
+  var processed = [];
+  var wordStart = false;
+  var word = [];
+  var wordString;
+  var nextWordModifier;
+  for (var i=0; i < line.length; i++) {
+    char = line[i];
+    code = char.charCodeAt(0);
+    if (this.isWordChar(char)) {
+      if (!wordStart) {
+        wordStart = true;
+      }
+      word.push(char);
+    } else {
+      if (wordStart) {
+        // End of word
+        wordString = word.join('');
+        processed.push(this.getWord(wordString, nextWordModifier));
+        nextWordModifier = this.nextWordRule(wordString);
+        wordStart = false;
+        word = [];
+
+        // Process next words in current block leve as method names
+        if (wordString === 'class') {
+          this.classLevel = this.openBlockLevel + 1;
+          this.isClass = true;
+        }
+
+        if (wordString === 'function') {
+          this.isFunctionDefinition = true;
+        }
+      }
+
+      switch (char) {
+        case '(':
+          if (this.isClassMethod() || this.isFunctionDefinition) {
+            this.isArguments = true;
+          }
+          break;
+        case ')':
+          if (this.isClassMethod() || this.isFunctionDefinition) {
+            this.isArguments = false;
+          }
+          break;
+        case '{':
+          this.openBlockLevel++;
+          this.isFunctionDefinition = false;
+          break;
+        case '}':
+          if (this.isClassMethod()) {
+            this.isClass = false;
+          }
+          this.openBlockLevel--;
+          break;
+        case '=':
+          var next = line[i+1];
+          if (next && next === '>') {
+            var nextChar;
+            var k = 1;
+            while (!nextChar) {
+              if (! (/\s/.test(line[i+1+k]))) {
+                nextChar = line[i+1+k]
+              } else {
+                k++;
+              }
+            }
+            // Is arrow function
+            if (nextChar === '{') {
+              processed.push(this.applyStyle('=>', 'b_name'));
+              //skip next char
+              i++;
+              continue;
+            }
+          }
+          break;
+      }
+
+      processed.push(this.getCharStyle(char));
+    }
+  }
+
+  return processed.join('');
+}
+
+
+SimpleHighlight.isClassMethod = function() {
+  return this.isClass && this.classLevel === this.openBlockLevel;
+}
+
+SimpleHighlight.getWord = function(word, customStyle) {
+  var style = this.keywords[word] || customStyle; 
+  if (!style) {
+    if (this.isArguments) {
+      style = 'args';
+    } else  if (this.isClassMethod()) {
+      // Methods
+      style = 'method';
+    } else if (/^[0-9]+$/.test(word)) {
+      // Numbers
+      style = 'number'
+    } else if(/^[\'\"].*[\'\"]+$/.test(word)) {
+      // Strings
+      style = 'string'
+    }
+  }
+
+  if (style) {
+    return this.applyStyle(word, style);
+  } else {
+    return word;
+  }
+}
+
+SimpleHighlight.applyStyle = function(word, style) {
+  return ['<span class="sh-',style,'">',word,'</span>'].join('');
+}
+
+SimpleHighlight.nextWordRule = function(keyword) {
+  if (this.modifiers.hasOwnProperty(keyword)) {
+    return this.modifiers[keyword];
+  } else {
+    return null;
+  }
+}
+
+SimpleHighlight.isWordChar = function(char) {
+  return /[a-zA-Z0-9\_\-\'\"]/.test(char)
+};
+
+SimpleHighlight.getCharStyle = function(char) {
+  if (/^[\+\-\*\|\/\>\<\=]+$/.test(char)) {
+    return this.applyStyle(char, 'operand');
+  } else {
+    return char;
+  }
+}
+
+SimpleHighlight.reset = function() {
+  this.openBlockLevel = 0;
+  this.classLevel = null;
+  this.isClass = false;
+  this.isArguments = false;
+  this.isFunctionDefinition = false;
+}
+
+SimpleHighlight.appendStyles = function(themes) {
+  var styleId = 'simple-higlight-styles';
+  // Styles aready added
+  if (document.getElementById(styleId)) {
+    return;
+  }
+
   var cssArr = [];
-  cssArr.push('code.simple-highlight { font-size: 16px; padding: 15px; display: block;}');
-  cssArr.push('.simple-highlight > pre { margin: 0}');
+  cssArr.push('code'+this.mainClass+' { font-size: 16px; padding: 15px; overflow-x: auto; display: block; margin: 5px; border-radius:4px}');
+  cssArr.push(this.mainClass + ' > pre { margin: 0}');
   for (var t = 0; t < themes.length; t++) {
-      createThemeCss(themes[t], cssArr);
+      this.createThemeCss(themes[t], cssArr);
   }
   var css = cssArr.join('\n');
   var head = document.head || document.getElementsByTagName('head')[0];
   var style = document.createElement('style');
   style.type = 'text/css';
+  style.id = styleId;
   if (style.styleSheet){
     style.styleSheet.cssText = css;
   } else {
@@ -130,8 +244,8 @@ function appendStyles(themes) {
   head.appendChild(style);
 }
 
-function createThemeCss(theme, css) {
-  var prefixName = '.simple-highlight.sh-theme-' + theme.name;
+SimpleHighlight.createThemeCss = function(theme, css) {
+  var prefixName = this.mainClass + '.sh-theme-' + theme.name;
   css.push(prefixName + ' { background: '+theme.background+'}');
   css.push(prefixName + ' > pre {color: '+theme.text+'}');
 
@@ -155,142 +269,67 @@ function createThemeCss(theme, css) {
   }
 }
 
-function isClassMethod() {
-  return isClass && classLevel === openBlockLevel;
-}
-
-function getWord(word, customStyle) {
-  var style = keywords[word] || customStyle; 
-  if (!style) {
-    if (isArguments) {
-      style = 'args';
-    } else  if (isClassMethod()) {
-      // Methods
-      style = 'method';
-    } else if (/^[0-9]+$/.test(word)) {
-      // Numbers
-      style = 'number'
-    } else if(/^[\'\"].*[\'\"]+$/.test(word)) {
-      // Strings
-      style = 'string'
-    }
+SimpleHighlight.themes = [{
+  name: 'dark',
+  background: '#282923',
+  text: '#f7f7f7',
+  colors: {
+    orange: '#fd9621',
+    green: '#a6e22c',
+    purple: '#ac80ff',
+    yellow: '#e7db74',
+    red: '#f92472',
+    blue: '#67d8ef',
+  },
+  styles: {
+    def: 'blue',
+    args: 'orange',
+    method: 'green',
+    number: 'purple',
+    string: 'yellow',
+    operand: 'red',
+    keyword: 'red',
+    b_name: 'blue|i',
+    g_name: 'blue|i'
   }
+}, {
+  name: 'light',
+  background: '#eee',
+  text: '#222',
+  colors: {
+    orange: '#fd9621',
+    green: '#a6e22c',
+    purple: '#ac80ff',
+    yellow: '#e7db74',
+    red: '#f92472',
+    blue: '#67d8ef',
+    grey: '#555'
+  },
+  styles: {
+    def: 'red',
+    args: 'orange|b',
+    method: 'purple',
+    number: 'green',
+    string: 'green',
+    operand: 'red',
+    keyword: 'red',
+    b_name: 'grey',
+    g_name: 'grey'
+  }
+}];
 
-  if (style) {
-    return applyStyle(word, style);
+// Hightlight initially on dom ready
+document.addEventListener("DOMContentLoaded", function() {
+  SimpleHighlight.highlightOnPage();
+});
+
+// Export public method
+globalObject.SimpleHighlight = function(node) {
+  if (node) {
+    SimpleHighlight.highlightCodeNode(node);
   } else {
-    return word;
+    SimpleHighlight.highlightOnPage();
   }
-}
-
-function applyStyle(word, style) {
-  return ['<span class="sh-',style,'">',word,'</span>'].join('');
-}
-
-function nextWordRule(keyword) {
-  if (modifiers.hasOwnProperty(keyword)) {
-    return modifiers[keyword];
-  } else {
-    return null;
-  }
-}
-
-function isWordChar(char) {
-  return /[a-zA-Z0-9\_\-\'\"]/.test(char)
 };
 
-function getCharStyle(char) {
-  if (/^[\+\-\*\|\/\>\<\=]+$/.test(char)) {
-    return applyStyle(char, 'operand');
-  } else {
-    return char;
-  }
-}
-
-function processLine(line) {
-  var char;
-  var code;
-  var processed = [];
-  var wordStart = false;
-  var word = [];
-  var wordString;
-  var nextWordModifier;
-  for (var i=0; i < line.length; i++) {
-    char = line[i];
-    code = char.charCodeAt(0);
-    if (isWordChar(char)) {
-      if (!wordStart) {
-        wordStart = true;
-      }
-      word.push(char);
-    } else {
-      if (wordStart) {
-        // End of word
-        wordString = word.join('');
-        processed.push(getWord(wordString, nextWordModifier));
-        nextWordModifier = nextWordRule(wordString);
-        wordStart = false;
-        word = [];
-
-        // Process next words in current block leve as method names
-        if (wordString === 'class') {
-          classLevel = openBlockLevel + 1;
-          isClass = true;
-        }
-
-        if (wordString === 'function') {
-          isFunctionDefinition = true;
-        }
-      }
-
-      switch (char) {
-        case '(':
-          if (isClassMethod() || isFunctionDefinition) {
-            isArguments = true;
-          }
-          break;
-        case ')':
-          if (isClassMethod() || isFunctionDefinition) {
-            isArguments = false;
-          }
-          break;
-        case '{':
-          openBlockLevel++;
-          isFunctionDefinition = false;
-          break;
-        case '}':
-          if (isClass && classLevel == openBlockLevel) {
-            isClass = false;
-          }
-          openBlockLevel--;
-          break;
-        case '=':
-          var next = line[i+1];
-          if (next && next === '>') {
-            var nextChar;
-            var k = 1;
-            while(!nextChar) {
-              if (! (/\s/.test(line[i+1+k]))) {
-                nextChar = line[i+1+k]
-              } else {
-                k++;
-              }
-            }
-            // Is arrow function
-            if (nextChar === '{') {
-              processed.push(applyStyle('=>', 'b_name'));
-              //skip next char
-              i++;
-              continue;
-            }
-          }
-          break;
-      }
-      
-
-      processed.push(getCharStyle(char));
-    }
-  }
-
-  return processed.join('');
-}
+})(window);
